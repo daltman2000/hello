60903847a5cb560cd53b319231aa14e8009d97f8
daltman2000 <daltman2000@gmail.com>
2019-01-17 14:46:29 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Dalton Evans
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Dalton. \n");
+	printf("My major is Electrical Engineering \n");
+	
+  return 0;
+}
